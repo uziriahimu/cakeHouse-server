c57a7fa190a229c8b8264f47af6d053fb69fcafc
uziriahimu <uziriahimu@gmail.com>
2022-11-09 23:05:42 +0600

    reviews post

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 require('dotenv').config()
 
@@ -18,12 +18,40 @@ async function run() {
 
     try {
         const serviceCollection = client.db('cakeHouse').collection('services')
+        const reviewCollection = client.db('cakeHouse').collection('reviews')
 
+
+        app.get('/home', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const user = await cursor.limit(3).toArray();
+            res.send(user)
+        })
         app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query)
             const user = await cursor.toArray();
             res.send(user)
+        })
+
+        app.get('/services/:id', async (req, res) => {
+
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+
+        })
+
+
+
+        app.post('/reviews', async (req, res) => {
+
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review)
+
+            res.send(result);
+
         })
     }
 
